521d6f32f6394e1453de2a59d5b5adb237a59649
你的名字啊 <brandon121888@gmail.com>
2020-07-14 11:29:45 +0800

    test

---
+++ test3/assets/Scripts/GameManagerB.js
@@ -0,0 +1,26 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+       
+
+       currentNum : 1,
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
